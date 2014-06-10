1bde74bb61b0c774616bbcd51ffc2f6442f2e4cb
Nathan Jervis <mirhagk@gmail.com>
2014-06-10 07:47:21 -0400

    Adding TSharp experiemental project

---
+++ TSharp/TSharp/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(
+@"using System;
+            using System.Collections;
+            using System.Linq;
+            using System.Text;
+
+namespace HelloWorld
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine(""Hello, World!"");
+            }
+        }
+    }");
+
+            var root = (CompilationUnitSyntax)tree.GetRoot();
+            
+            WriteTree(root);
+            Console.ReadKey();
+        }
+        static string Repeat(string text, int times)
+        {
+            string result = "";
+            for (int i = 0; i < times; i++) result += text;
+            return result;
+        }
+        static void WriteTree(SyntaxNode node, int indent=0)
+        {
+            Console.WriteLine("{0}----------------\n{0}{2}-{1}", Repeat(".", indent), node,node.CSharpKind());
+            foreach (var child in node.ChildNodes())
+                WriteTree(child, indent + 1);
+        }
+    }
+}
