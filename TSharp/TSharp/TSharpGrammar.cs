b54227a13df9bac9b062b3da2794b8485fe64380
Nathan Jervis <mirhagk@gmail.com>
2014-08-19 07:57:57 -0400

    Added GenerateMemberAccess from Roslyn code.
    First step to be able to call .NET functions from within the code.

--- TSharp/TSharp/TSharpGrammar.cs
+++ TSharp/TSharp/TSharpGrammar.cs
@@ -16,13 +16,38 @@ namespace TSharp
             var result = "";
             foreach (var item in input) result += item;
             return result;
+        }
+        public static void Test()
+        {
 
 
+            Console.WriteLine("test");
+            var invokable = SyntaxFactory.InvocationExpression(GenerateMemberAccess("System", "Console", "WriteLine"));
+            var lambda = SyntaxFactory.SimpleLambdaExpression(SyntaxFactory.Parameter(SyntaxFactory.Identifier("x")), invokable);
             
             //SyntaxFactory.PredefinedType
             //SyntaxFactory.V
             //SyntaxFactory.ExpressionStatement()
             //SyntaxFactory.LiteralExpression(SyntaxKind.NumericLiteralExpression
+        }
+
+        private static ExpressionSyntax GenerateMemberAccess(params string[] names)
+        {
+            ExpressionSyntax result = SyntaxFactory.IdentifierName(SyntaxFactory.Token(SyntaxKind.GlobalKeyword));
+            for (int i = 0; i < names.Length; i++)
+            {
+                var name = SyntaxFactory.IdentifierName(names[i]);
+                if (i == 0)
+                {
+                    result = SyntaxFactory.AliasQualifiedName((IdentifierNameSyntax)result, name);
+                }
+                else
+                {
+                    result = SyntaxFactory.MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression, result, name);
+                }
+            }
+
+            return result;
         }
     }
 }
